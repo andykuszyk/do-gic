7e8ee178f2e72e8d801fedb384a023dd180b0690
Andy Kuszyk <pairofsocks@hotmail.co.uk>
2020-04-19 14:30:08 +0100

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sed -i "s/\[gitlab-token\]/$gitlabToken/g" docker-compose.yml
+scp -i id_rsa -o "StrictHostKeyChecking no" ./docker-compose.yml $douser@$doip:~/docker-compose.yml
+ssh -i id_rsa -o "StrictHostKeyChecking no" $douser"$doip "docker stack deploy -c ~/docker-compose.yml szyk"
